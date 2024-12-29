86c384617cf1de018a2e3d1c8f863d74e0004619
Hunter <yuanxy92@users.noreply.github.com>
2024-12-29 23:19:56 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-python train.py --content_dir /data/hdd/Data/Metalens/MetalensSR_20241220/lr_images --style_dir /data/hdd/Data/Metalens/MetalensSR_20241220/hr_images
+python train.py --content_dir /data/hdd/Data/Metalens/MetalensSR_20241220/lr_images \
+    --style_dir /data/hdd/Data/Metalens/MetalensSR_20241220/hr_images
